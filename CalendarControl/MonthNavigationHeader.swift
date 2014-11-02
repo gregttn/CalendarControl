f616d0815ba3378d902986b98f905532fbe37fd0
gregttn <gregttn@gmail.com>
2014-11-02 20:51:03 +0000

    added placeholder buttons for navigation through months

--- CalendarControl/MonthNavigationHeader.swift
+++ CalendarControl/MonthNavigationHeader.swift
@@ -11,6 +11,14 @@ import UIKit
 class MonthNavigationHeader: UICollectionReusableView {
     var title = UILabel()
     
+    lazy private var next: UIButton = {
+        return self.navigationButton(">>")
+    }()
+    
+    lazy private var previous: UIButton = {
+        return self.navigationButton("<<")
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame);
         build()
@@ -26,5 +34,25 @@ class MonthNavigationHeader: UICollectionReusableView {
         title.textAlignment = NSTextAlignment.Center
         
         addSubview(title)
+        
+        addSubview(next)
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("[btn]|", options: NSLayoutFormatOptions.AlignAllLeft, metrics: nil, views: ["btn":next]))
+        
+        addSubview(previous)
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|[btn]", options: NSLayoutFormatOptions.AlignAllLeft, metrics: nil, views: ["btn":previous]))
+    }
+    
+    private func navigationButton(title: String) -> UIButton {
+        var button = UIButton()
+        button.setTitle(title, forState: UIControlState.Normal)
+        button.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+        button.setTitleColor(UIColor.lightGrayColor(), forState: UIControlState.Highlighted)
+        button.sizeToFit()
+        button.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        button.autoresizingMask = UIViewAutoresizing.FlexibleRightMargin
+        
+        
+        return button
     }
 }
